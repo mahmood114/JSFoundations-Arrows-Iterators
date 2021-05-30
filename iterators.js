17ad51b5d90e2a78d97992e2811890867560b4e5
mahmood114 <mahmoodalmohri@gmail.com>
2021-05-30 17:39:56 +0300

    Task completed

--- iterators.js
+++ iterators.js
@@ -5,7 +5,7 @@
  * - Logs every element of the array
  ************************************/
 const logger = function (array) {
-
+    array.forEach(element => console.log(element))
 };
 
 /*************************************
@@ -19,9 +19,7 @@ const logger = function (array) {
  * The conversion is:
  *   C = (F - 32) * (5/9)
  ************************************/
-const toCelsius = function (temperatures) {
-
-};
+const toCelsius = temperatures => temperatures.map(temp => (temp-32)*(5/9));
 
 /**************************************
  * hottestDays(temperatures, threshhold)
@@ -31,9 +29,7 @@ const toCelsius = function (temperatures) {
  * - Returns an array of temperatures
  *   that exceed the threshhold
  ***************************************/
-const hottestDays = function (temperatures, threshhold) {
-
-};
+const hottestDays = (temperatures, threshhold) => temperatures.filter(temp => temp > threshhold)
 
 /******************************************
  * logHottestDays(temperatures, threshhold)
@@ -49,9 +45,7 @@ const hottestDays = function (temperatures, threshhold) {
  * hint: you can combine
  *       all previous functions
  *******************************************/
-const logHottestDays = function (temperatures, threshhold) {
-
-};
+const logHottestDays = (temperatures, threshhold) => logger(toCelsius(hottestDays(temperatures, threshhold)));
 
 // const logHottestDays = function (temperatures, threshhold) {
 //   temperatures
